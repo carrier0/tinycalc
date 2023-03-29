2bd8d9af4df3e7b4ff5d061c81a2a505ad95c2df
s1ush3r <jedurko@live.com>
2023-03-29 18:40:50 -0400

    updating binary

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # tinycalc
-Console-based calculator program that fits into the minimum Windows file size of 268 bytes. Tested from Windows XP SP2 to Windows 11.
+Console-based calculator program that fits into the minimum Windows 10 Portable Executable size of 268 bytes. Tested from Windows XP SP2 to Windows 10.
+
+# Use and caveats
+The four basic operations (addition, subtraction, multiplication, division) are supported. There is no real argument parsing that I could fit into the program, so it relies on jumping 10 characters forward from the start of the command line input to get to the argument. For example:
+- **(64-bit Windows)** tinycalc 25*10: outputs 250
+- **(32-bit Windows)** tinycalc  25*10 (two spaces): outputs 250
+- **(32-bit Windows)** tinycalc 25*10 (one space): outputs 50
+
+Numerous parts of the PE header are overwritten and jumped over in order to fit the code. An ordinal import of `printf` is used for output, along with another ordinal import of the `exit` to avoid a crash message on XP.
--- tinycalc.asm
+++ tinycalc.asm
@@ -3,133 +3,135 @@ base equ 0x400000
 
 ;IMAGE_DOS_HEADER
 dos:
-	dw "MZ"       	; e_magic
-	dw 0		; e_cblp
+	dw "MZ"       			; e_magic
+	dw 0				; e_cblp
 
 ;IMAGE_NT_HEADERS
 ntsig:
-	dd "PE"
+	dd "PE"				; Signature
 
-;IMAGE_FILE_HEADERS
+;IMAGE_FILE_HEADER
 filehdr:
-	dw 0x014C            ; Machine
-	dw 1                 ; NumberOfSections
+	dw 0x014C            		; Machine
+	dw 1                 		; NumberOfSections
 	
 	init:
-	xor esi, esi	     ; PointerToSymbolTable 
-	mov eax, [fs:30h+esi]; NumberOfSymbols
-	mov eax, [eax+10h]   ; NumberOfSymbols
-	jmp init_2
+	xor esi, esi	     		; OVERWRITTEN:
+	mov eax, [fs:30h+esi]		; PointerToSymbolTable, 
+	mov eax, [eax+10h]   		; NumberOfSymbols,
+	jmp init_2                      ; NumberOfSymbols
 	db 0 
 
-	dw opthdrsize        ; SizeOfOptionalHeader
-	dw 0x103             ; Characteristics
+	dw opthdrsize        		; SizeOfOptionalHeader
+	dw 0x103             		; Characteristics
+
 
 ;IMAGE_OPTIONAL_HEADER
 opthdr:
-	dw 0x10b                    ; Magic
+	dw 0x10b                    	; Magic
 
 	init_2:			    
-	mov esi, [eax+44h]          ; OVERWRITTEN: MajorLinkerVersion
-	add esi, 20                 ; MinorLinkerVersion,
-	cld                         ; SizeOfCode,
-	xor ecx, ecx                ; SizeOfInitializedData,
-	jmp precalc                    ; SizeOfUninitializedData
+	mov esi, [eax+44h]          	; OVERWRITTEN: MajorLinkerVersion,
+	add esi, 20                 	; MinorLinkerVersion,
+	cld                         	; SizeOfCode,
+	xor ecx, ecx                	; SizeOfInitializedData,
+	jmp precalc                 	; SizeOfUninitializedData
 	format: db "%d", 0
-	
 
-	; Storing address of UNICODE_STRING PEB struct into
-	; esi, which is equivalent to GetCommandLineW
+	; Storing the address of UNICODE_STRING from PEB
+	; into esi - equivalent to GetCommandLineW
+	; No real argument parsing
 
-	dd init                     ; AddressOfEntryPoint
+	dd init                     	; AddressOfEntryPoint
 	
 precalc:
-	xor edx, edx
-	xor eax, eax
-	jmp calc_1
-
-	dw 0		    ; BaseOfCode
-	;dd 0                        ; BaseOfData
+	xor edx, edx			; OVERWRITTEN:
+	xor eax, eax			; 
+	jmp calc_1			; BaseOfCode,
+	dw 0		    		; BaseOfData
 	
-	; more required sections
-	dd base                     ; ImageBase
-	dd 4			    ; e_lfanew  ; SectionAlignment
-	dd 4                        ; FileAlignment
+	; More required sections
+	dd base                     	; ImageBase
+	dd 4			    	; e_lfanew  ; SectionAlignment
+	dd 4                        	; FileAlignment
 
 calc_1:
-	lodsw                       ; MajorOperatingSystemVersion
-	test eax, eax               ; MinorOperatingSystemVersion
-	je finish                   ; MajorImageVersion
-	jmp calc_2               ; MinorImageVersion
+	lodsw                       	; OVERWRITTEN:
+	test eax, eax               	; MajorOperatingSystemVersion,
+	je finish                   	; MinorOperatingSystemVersion,
+	jmp calc_2               	; MajorImageVersion, MinorImageVersion
 	
+	; Next character loaded into eax and tested for null
 
-	dw 4                        ; MajorSubsystemVersion
+	dw 4                        	; MajorSubsystemVersion
 
 	opset:
-	mov ebx, eax                ; MinorSubsystemVersion
-	mov ecx, edx                ; Win32VersionValue
-	jmp precalc
+	mov ebx, eax                	; OVERWRITTEN:
+	mov ecx, edx                	; MinorSubsystemVersion,
+	jmp precalc                     ; Win32VersionValue
 
-	dd 1024                     ; SizeOfImage
-	dd 1			    ; SizeOfHeaders          nonzero for Windows XP
-	dd 0			    ; CheckSum
-	dw 3                        ; Subsystem (Console)
-	dw 0                        ; DllCharacteristics
-	dd 0                        ; SizeOfStackReserve
-	dd 0                        ; SizeOfStackCommit
-	dd 0                        ; SizeOfHeapReserve
+	; Possible operation character stored into ebx and then
+	; skipped over
+
+	; Difficult to overwrite sections
+	dd 1024                     	; SizeOfImage
+	dd 1			    	; SizeOfHeaders
+	dd 0			    	; CheckSum
+	dw 3                        	; Subsystem (Console)
+	dw 0                        	; DllCharacteristics
+	dd 0                        	; SizeOfStackReserve
+	dd 0                        	; SizeOfStackCommit
+	dd 0                        	; SizeOfHeapReserve
 	
-	addnum:
-	sub eax, 0x30
-	add edx, eax
+	addnum:				; OVERWRITTEN:
+	sub eax, 0x30			; SizeOfHeapCommit,
+	add edx, eax			; LoaderFlags
 	jmp calc_1
-	db 0        		    ; SizeOfHeapCommit
+	db 0
 
-	dd 2                        ; NumberOfRvaAndSizes
+	dd 2                        	; NumberOfRvaAndSizes
 
-; Data directories (part of optional header)
+; IMAGE_DIRECTORY_ENTRIES
 calc_2:
 	cmp eax, 0x3A
-	jge badexit
-	jmp calc_3
+	jge fail			; OVERWRITTEN:
+	jmp calc_3			; EXPORT
 	db 0
-				    ; EXPORT
-	dd idata, 0                 ; IMPORT
+
+	dd idata, 0			; IMPORT
 
 opthdrsize equ $ - opthdr
 
 ; IMAGE_SECTION_HEADER
 calc_3:
 	cmp eax, 0x30
-	jl opset
-	jmp calc_4
-	db 0			    ; Name
+	jl opset			; OVERWRITTEN:
+	jmp calc_4			; Name
+	db 0
 
-	dd codesize                 ; VirtualSize
-	dd init                     ; VirtualAddress
-	dd codesize                 ; SizeOfRawData
-	dd init                     ; PointerToRawData
-
-	; Jumping over required headers
+	dd codesize                 	; VirtualSize
+	dd init                     	; VirtualAddress
+	dd codesize                 	; SizeOfRawData
+	dd init                     	; PointerToRawData
 
 	calc_4:
-	imul edx, 0x0A
-	jmp addnum
-
-finish:
-	cmp ebx, '+'
+	imul edx, 0x0A			; OVERWRITTEN:
+	jmp addnum			; PointerToRelocations,
+					; PointerToLineNumbers,
+finish:					; NumberOfRelocations,
+	cmp ebx, '+'			; NumberOfLineNumbers
 	je addop
 	jmp fin2
 
-	dd 0
-
+	dd 0				; Characteristics
+					; Must remain 0 for XP compatibility
 	fin2:
 	cmp ebx, '-'
 	je subop
 	cmp ebx, '*'
 	je mulop
 	cmp ebx, '/'
-	jne badexit
+	jne fail
 
 	divop:
 	mov eax, ecx
@@ -153,23 +155,21 @@ printnum:
 	jmp idata
 
 crt:
-	db "crtdll", 0x00
-
+	db "crtdll", 0x00		; Importing by ordinal using the shortest
+					; named C Runtime dll
 ; Import Address Table
 iat:
 printf:	dd 0x800001b8
 exit:	dd 0x80000167
         dd 0x00
 
-; Import table (array of IMAGE_IMPORT_DESCRIPTOR structures)
-idata:
-	print:
-	call [base + printf]	; OriginalFirstThunk UNUSED
-	badexit:		; TimeDateStamp and ForwarderChain UNUSED
-	call [base + exit]
-	dd crt                      ; Name
-	dd iat                      ; FirstThunk
+; Import Table
+idata:					; OVERWRITTEN:
+print:	call [base + printf]		; OriginalFirstThunk,
+fail:	call [base + exit]		; TimeDateStamp, ForwarderChain
+
+	dd crt                      	; Name
+	dd iat                      	; FirstThunk
 
 codesize equ $ - init
-
 times 268 - ($-$$) db 0
